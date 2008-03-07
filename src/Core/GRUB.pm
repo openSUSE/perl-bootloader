e239a3ec8d881889b46c44647ff08cbec3843e3c
Alexander Osthof <aosthof@suse.de>
2008-03-07 13:07:50 +0000

    - Changed some l_debug output to l_milestone to be always present - Added some additional milestones - Added a fallback for grub devices if translation fails - Version bump to 0.4.18.15
    svn path=/branches/SLES10-SP1/; revision=474

--- src/Core/GRUB.pm
+++ src/Core/GRUB.pm
@@ -472,12 +472,14 @@ sub UnixDev2GrubDev {
 	$kernel_dev = $dev;
     }
     
+    $self->l_milestone ("GRUB::UnixDev2GrubDev: kernel device: $kernel_dev");
+
     my $partition = undef;
     if ($dev =~ m#/dev/md\d+#) {
 	my @members = @{$self->MD2Members ($dev) || []};
 	# FIXME! This only works for mirroring (Raid1)
 	$kernel_dev = $members[0] || $kernel_dev;
-	$self->l_debug ("GRUB::UnixDev2GrubDev: First device of MDRaid:: $original --> $kernel_dev");
+	$self->l_milestone ("GRUB::UnixDev2GrubDev: First device of MDRaid:: $original --> $kernel_dev");
     }
     elsif (($dev =~ m{/dev/evms/}) and ($dev !~ m{/dev/evms/lvm2?/})) {
 	# This uses the naming scheme used by EVMS for no lvm devices which is
@@ -485,7 +487,7 @@ sub UnixDev2GrubDev {
 	# the future.
 	$kernel_dev = $dev;
 	$kernel_dev =~ s{^/dev/evms/}{/dev/}; # map back to kernel device name
-	$self->l_debug ("GRUB::UnixDev2GrubDev: Mapped EVMS to kernel device: $original --> $kernel_dev");
+	$self->l_milestone ("GRUB::UnixDev2GrubDev: Mapped EVMS to kernel device: $original --> $kernel_dev");
     }
 
     # fetch the underlying device (sda1 --> sda)
@@ -518,10 +520,33 @@ sub UnixDev2GrubDev {
 	}
     }
 
+    # print all entries of device.map. This is rather for debugging
+    if (exists $self->{"device_map"}) {
+	$self->l_milestone ("GRUB::UnixDev2GrubDev: Read from internal structure device_map:");
+
+	while ((my $unix_dev, my $grub_dev) = each (%{$self->{"device_map"}})) {
+	    $self->l_milestone ("unix device: $unix_dev <==> grub device: $grub_dev");
+	}
+    }
+    else {
+	$self->l_milestone ("GRUB::UnixDev2GrubDev: Internal structure device_map doesn't exist.");
+    }
+
+    $self->l_milestone ("GRUB::UnixDev2GrubDev: Translated UNIX
+	device/partition -> GRUB device: $original to $dev");
+
+    # fallback to grub device hd0 if translation has failed - this is good
+    # enough for many cases
+    if ($dev !=~ /^hd\d+$/) {
+	$dev = "hd0";
+    }
+
     $dev = defined ($partition)
 	? "($dev,$partition)"
 	: "($dev)";
-    $self->l_debug ("GRUB::UnixDev2GrubDev: Translated UNIX->GRUB: $original to $dev");
+
+    $self->l_milestone ("GRUB::UnixDev2GrubDev: Translated UNIX->GRUB: $original to $dev");
+
     return $dev;
 }
 
