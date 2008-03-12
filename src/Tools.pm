52378871f0bdb867fea18633114818a7fc204c0f
Alexander Osthof <aosthof@suse.de>
2008-03-12 14:13:32 +0000

    - Fixed global default label for lilo (#361079) - Version bump to 0.4.48
    svn path=/trunk/; revision=478

--- src/Tools.pm
+++ src/Tools.pm
@@ -1303,7 +1303,7 @@ sub AddSection {
 
     my $loader = Bootloader::Tools::GetBootloader ();
 
-    if ($loader ne "grub") {
+    if ($loader ne "grub" and $loader ne "lilo") {
         # Search for duplicate boot entry label and rename them in a unique way
 	foreach my $s (@sections) {
 	    while ((my $k, my $v) = each (%$s)) {
@@ -1671,7 +1671,7 @@ sub RemoveSections {
     }
 
     $lib_ref->SetSections (\@sections);
-    if ($default_removed) {
+    if ($default_removed or $loader eq "lilo") {
 	$glob_ref->{"default"} = $sections[0]{"name"};
 
 	# During an update with YaST running in the inst-sys, if the default
