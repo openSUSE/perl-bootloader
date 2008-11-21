4b41052e3f2607dcfc949c65f3ec2bb035a792c2
Jiří Šrain <jsrain@suse.cz>
2008-11-21 13:03:11 +0000

    load initrd at 16MB on S/390 (bnc #434234)
    svn path=/trunk/; revision=649

--- src/Core/ZIPL.pm
+++ src/Core/ZIPL.pm
@@ -967,7 +967,7 @@ sub CreateSingleMenuFileLine {
     }
     elsif (($key eq "ramdisk" || $key eq "initrd") && !($value =~ /,0x/))
     {
-        return "$key$equal_sep$value,0x1000000";
+        return "$key$equal_sep$value,0x2000000";
     }
     else
     {
