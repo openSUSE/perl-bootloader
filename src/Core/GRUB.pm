2087bd86e22dd66072242bc7cf0a1d7004bca504
Josef Reidinger <jreidinger@suse.cz>
2010-03-23 15:27:26 +0000

    increase log priority for translating unix path to grub path
    svn path=/branches/SLE11-SP1/; revision=847

--- src/Core/GRUB.pm
+++ src/Core/GRUB.pm
@@ -709,7 +709,7 @@ sub UnixPath2GrubPath {
     }
 
     $path = $dev . $path;
-    $self->l_debug ("GRUB::UnixPath2GrubPath: Translated path: $orig_path, prefix $preset_dev, to: $path");
+    $self->l_milestone ("GRUB::UnixPath2GrubPath: Translated path: $orig_path, prefix $preset_dev, to: $path");
     return $path;
 }
 
