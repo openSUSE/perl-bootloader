dc15a339ae654753c02138b9e84e21d2440f48a8
Josef Reidinger <jreidinger@suse.cz>
2010-03-31 10:42:09 +0000

    fix key for check presence
    svn path=/branches/SLE11-SP1/; revision=854

--- src/Core/ZIPL.pm
+++ src/Core/ZIPL.pm
@@ -828,7 +828,7 @@ sub PrepareMenuFileLines {
            }
        }
        my $is_dump = (($sect->{"type"} eq "dump") and
-         (defined $sect->{"type"}) and ($sect->{"dumpto"} ne ""));
+         (defined $sect->{"dumpto"}) and ($sect->{"dumpto"} ne ""));
        if (!$found and $sect->{"name"} ne "menu" and !$is_dump) {
            push @sect_names_to_append, $sect->{"name"};
        }
